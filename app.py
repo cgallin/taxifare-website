0e71845bb301b14681792bbb0ff5d473d7dd03ea
Cameron Gallinger <cam.gallinger@gmail.com>
2024-11-29 11:42:16 -0500

    My first website

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 '''
 #TaxiFareModel front
 '''
@@ -6,11 +7,12 @@ pickup_longitude=st.number_input('pickup_longitude')
 pickup_latitude=st.number_input('pickup_latitude')
 dropoff_longitude=st.number_input('dropoff_longitude')
 dropoff_latitude=st.number_input('dropoff_latitude')
-passenger_count=st.integer_input('passenger_count',min_value=1, max_value=8)
-st.button('Predict')
+passenger_count = st.slider('Select a line count', 1, 6,1)
+
+
+
 
 url = 'https://taxifare.lewagon.ai/predict'
-
 dict = {
     "pickup_longitude": pickup_longitude,
     "pickup_latitude": pickup_latitude,
@@ -19,5 +21,6 @@ dict = {
     "passenger_count": passenger_count
 }
 
-prediction = requests.get(url, params=dict).json()
-st.write("Predicted Cost", prediction['prediction'])
+if st.button('Predict'):
+    prediction = requests.get(url, params=dict).json()
+    st.write("Predicted Cost", prediction['prediction'])
